5bec258931a0625109421a1c358eec2da64dcf08
Asus <Asus@DESKTOP-F0SLSJ8>
2022-03-31 18:05:38 +0300

    1

---
+++ EX2/Program.cs
@@ -0,0 +1,13 @@
+﻿int[,] second = { { 44, 56, 18 }, { 67, 45, 89 }, {126, 23,76 } };
+for (int i = 0; i < 3; i++)
+{   
+    int max = 0;
+    for (int j = 0; j < 3; j++)
+    {
+        if (second[i, j] > max)
+        {
+            max = second[i, j];
+        }
+    }
+    Console.WriteLine($"Максимальный элемент в {i+1} строке: {max}");
+}
---
+++ HW7/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] first = new int[6];
+for (int i = 0; i< first.Length; i++)
+{
+    Console.WriteLine($"Введите {i} элемент массива");
+    first[i] = int.Parse(Console.ReadLine());
+}
+for (int b = 0; b < first.Length; b++)
+{
+    for (int i = 0; i < first.Length - 1; i++)
+    {
+        if (first[i] < first[i + 1])
+        {
+            int c = first[i];
+            first[i] = first[i + 1];
+            first[i + 1] = c;
+        }
+    }
+}
+for (int i = 0; i < first.Length; i++)
+{
+    Console.WriteLine(first[i]);
+}
